40f26b73b2fa7abd9de0f708b4c481d9f1586fb2
109534201 <129161993+109534201@users.noreply.github.com>
2023-11-24 17:27:24 +0800

    test

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>淨灘報名資訊系統</title>
+<style type="text/css">
+body {
+	background-color: #D0D3E4;
+}
+img{
+	border: 8px solid #757DA8;
+	border-radius: 20px;
+	}
+
+.bt1 {
+	height: 100px;
+	width: 500px;
+	border: 5px solid #757DA8;
+	border-radius: 20px;
+	font-family: "標楷體";
+	font-size: 18px;
+	color: #757DA8;
+	font-weight: bolder;
+	cursor: pointer;
+	box-shadow: 0px 5px 5px 0.5px #666;
+}
+</style>
+<link href="SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css">
+<script src="SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+</head>
+<body>
+<div align="center">
+  <ul id="MenuBar1" class="MenuBarHorizontal">
+    <li><a class="MenuBarItemSubmenu" href="#">項目 1</a>
+      <ul>
+        <li><a href="#">項目 1.1</a></li>
+        <li><a href="#">項目 1.2</a></li>
+        <li><a href="#">項目 1.3</a></li>
+      </ul>
+    </li>
+    <li><a href="#" style="background-color:#A5ABCA">項目 2</a></li>
+    <li><a class="MenuBarItemSubmenu" href="#">項目 3</a>
+      <ul>
+        <li><a class="MenuBarItemSubmenu" href="#">項目 3.1</a>
+          <ul>
+            <li><a href="#">項目 3.1.1</a></li>
+            <li><a href="#">項目 3.1.2</a></li>
+          </ul>
+        </li>
+        <li><a href="#">項目 3.2</a></li>
+        <li><a href="#">項目 3.3</a></li>
+      </ul>
+    </li>
+    <li><a href="#" style="background-color:#A5ABCA">項目 4</a></li>
+  </ul>
+  <br/>
+   <br/>
+</div>
+<div align="center">
+<h1 style="color:#757DA8">&#10024;最&nbsp;新&nbsp;消&nbsp;息&#10024;
+  </h1>
+  <img src="image1.jpeg" width="451" height="300" >
+</div>
+<br/>
+<br/>
+<br/>
+<div align="center">
+<!--
+https://www.sow.org.tw/2023oceanevent/coastalcleanup
+-->
+<button class="bt1">
+花蓮漁港北邊海灘
+</button>
+</div>
+<script type="text/javascript">
+var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {imgDown:"SpryAssets/SpryMenuBarDownHover.gif", imgRight:"SpryAssets/SpryMenuBarRightHover.gif"});
+  </script>
+</body>
+</html>
