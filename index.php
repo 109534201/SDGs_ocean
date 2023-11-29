939de05d3300fa575f5f830512753673b808689a
109534201 <129161993+109534201@users.noreply.github.com>
2023-11-29 10:30:04 +0800

    test

---
+++ index.php
@@ -0,0 +1,43 @@
+<!--
+<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2Fo3X4Kp89y8gSrhv0fVu9K6%2FSDGS-%25E6%25B5%25B7%25E6%25B4%258B%25E9%2581%258A%25E6%2588%25B2%3Ftype%3Ddesign%26node-id%3D0%253A1%26mode%3Ddesign%26t%3D7NX2ashd05sgT0A4-1" allowfullscreen></iframe>
+https://109534201.github.io/SDGs_ocean/
+-->
+
+<?php
+include "mane.html";
+?>
+
+ 
+<div class="main">
+  <div align="center" >
+    <h1 style="color:#757DA8">&#10024;最&nbsp;新&nbsp;消&nbsp;息&#10024;
+    </h1>
+    <img src="image/image2.jpeg" width="451" height="300">
+  </div>
+  <br />
+  <br />
+  <br />
+  <div align="center">
+    <!--
+https://www.sow.org.tw/2023oceanevent/coastalcleanup
+-->
+    <button class="bt1">
+      花蓮漁港北邊海灘
+    </button>
+    <br /><br /><br />
+    <button class="bt1">
+      台南市觀夕平台
+    </button>
+    <br /><br /><br />
+    <button class="bt1">
+      宜蘭縣竹安溪口濕地
+    </button>
+  </div><br />
+
+</div>
+  <script type="text/javascript">
+    var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", { imgDown: "SpryAssets/SpryMenuBarDownHover.gif", imgRight: "SpryAssets/SpryMenuBarRightHover.gif" });
+  </script>
+</body>
+
+</html>
